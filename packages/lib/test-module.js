4724de8afbb434df2f334ca9dff3ad22497b8da9
Woolum <woolumc@amazon.com>
2022-01-07 10:21:16 -0800

    create repro

---
+++ packages/lib/test-module.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doSomething = void 0;
+const Components = require("@aws-amplify/ui-react/dist/primitives.json");
+function doSomething() {
+    return Components;
+}
+exports.doSomething = doSomething;
---
+++ packages/lib/test-module.ts
@@ -0,0 +1,5 @@
+const Components = require("@aws-amplify/ui-react/dist/primitives.json");
+
+export function doSomething() {
+  return Components;
+}
